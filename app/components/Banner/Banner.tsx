3493cc7c54fcf65e5368871d7017bbbbdc8c2bde
MadePutraSanjaya <madeputra.6166@gmail.com>
2024-02-24 22:25:35 +0800

    fix: bug hardcode banner

--- app/components/Banner/Banner.tsx
+++ app/components/Banner/Banner.tsx
@@ -9,36 +9,33 @@ import CardInfo from '../Cards/CardInfo/CardInfo';
 const Banner: React.FC = () => {
   return (
     <>
-    <div className="columns-2">
-      <div className="text-banner relative top-32 lg:top-40">
-        <h1 className="text-3xl font-semibold mb-4 w-4/5">
-          Hello, I’m Made Sanjaya Front End Developer Based In Bali{" "}
-        </h1>
-        <p className="text-sm font-regular text-gray-500">
-          A Passionate Front End Developer With More 2 Years Of Experience Who
-          Is Always Excited to work with you to creating wonderful Website
-        </p>
-        <div className="flex flex-row mt-9 gap-4">
-          <div className="">
-            <CTA className="button-primary"> 
-               Hire Me
-            </CTA>
+      <div className="columns-2">
+        <div className="text-banner relative top-32 lg:top-40">
+          <h1 className="text-3xl font-semibold mb-4 w-4/5">
+            Hello, I’m Made Sanjaya Front End Developer Based In Bali{" "}
+          </h1>
+          <p className="text-sm font-regular text-gray-500">
+            A Passionate Front End Developer With More 2 Years Of Experience Who
+            Is Always Excited to work with you to creating wonderful Website
+          </p>
+          <div className="flex flex-row mt-9 gap-4">
+            <div className="">
+              <CTA className="button-primary">Hire Me</CTA>
+            </div>
+            <div className="">
+              <CTA className="button-primary-outline">Download CV</CTA>
+            </div>
           </div>
-          <div className="">
-              <CTA className="button-primary-outline"> 
-               Download CV
-            </CTA>
-          </div>
+        </div>
+
+        <div className="image-profile">
+          <Image src={Profile} alt="Made Sanjaya" />
         </div>
       </div>
 
-      <div className="image-profile">
-        <Image src={Profile} alt="Made Sanjaya" />
-      </div>
-
-    <div className="flex flex-row gap-4">
-    <CardInfo />
-    </div>
+        <div className="flex flex-row gap-4">
+          <CardInfo />
+        </div>
     </>
   );
 }
