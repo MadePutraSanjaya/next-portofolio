c051fd34d1099d092ed96d9b857f52100bf30d02
MadePutraSanjaya <madeputra.6166@gmail.com>
2024-03-01 16:07:09 +0800

    feature: remove comments

--- components/ParticlesContainer/ParticlesContainer.tsx
+++ components/ParticlesContainer/ParticlesContainer.tsx
@@ -3,10 +3,7 @@ import { loadSlim } from "tsparticles-slim"; // loads tsparticles-slim
 //import { loadFull } from "tsparticles"; // loads tsparticles
 import { useCallback, useMemo } from "react";
 
-// tsParticles Repository: https://github.com/matteobruni/tsparticles
-// tsParticles Website: https://particles.js.org/
 const ParticlesComponent = (props) => {
-  // using useMemo is not mandatory, but it's recommended since this value can be memoized if static
   const options = useMemo(() => {
     return {
       fullScreen: { enable: false },
@@ -84,13 +81,10 @@ const ParticlesComponent = (props) => {
   }, []);
 
 
-  // useCallback is not mandatory, but it's recommended since this callback can be memoized if static
   const particlesInit = useCallback((engine) => {
     loadSlim(engine);
-    // loadFull(engine); // for this sample the slim version is enough, choose whatever you prefer, slim is smaller in size but doesn't have all the plugins and the mouse trail feature
   }, []);
 
-  // setting an id can be useful for identifying the right particles component, this is useful for multiple instances or reusable components
   return <Particles id={props.id} init={particlesInit} options={options} className="w-full h-full absolute translate-z-0" />;
 };
 
