d9224ea779bec316c1e0f7689289cd8f3e87657e
MadePutraSanjaya <madeputra.6166@gmail.com>
2024-03-04 17:11:17 +0800

    fix: scroll nav tabs responsive

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,8 +6,8 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../../variant";
 import { aboutData } from "./../../components/data/about";
 import CountUp from "react-countup";
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 import { FaArrowTrendUp } from "react-icons/fa6";
 
 const About = () => {
@@ -94,7 +94,7 @@ const About = () => {
           exit="hidden"
           className="flex flex-col w-full xl:max-w-[48%] h-[480px]"
         >
-          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-2 overflow-x-scroll w-96 xl:overflow-hidden md:w-auto pb-4 xl:pb-auto xl:w-max">
             {aboutData.map((item, itemIndex) => {
               return (
                 <div
@@ -113,89 +113,104 @@ const About = () => {
 
           <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
             {aboutData[index].info.map((item, itemIndex) => {
-    return (
-      <div
-        key={itemIndex}
-        className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
-      >
-        {/* skills */}
-        {item.title && (
-          <>
-            <div className="font-light mb-2 md:mb-0">{item.title}</div>
-            <div className="hidden md:flex">-</div>
-            <div className="flex gap-x-4">
-              {"icons" in item && (
-                <div key={itemIndex} className="text-2xl text-white">
-                  {(item as any).icons}
+              return (
+                <div
+                  key={itemIndex}
+                  className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
+                >
+                  {/* skills */}
+                  {"title" in item && item.title !== null && (
+                    <>
+                      <div className="font-light mb-2 md:mb-0">
+                        {item.title}
+                      </div>
+                      <div className="hidden md:flex">-</div>
+                      <div className="flex gap-x-4">
+                        {"icons" in item && (
+                          <div key={itemIndex} className="text-2xl text-white">
+                            {(item as any).icons}
+                          </div>
+                        )}
+                      </div>
+                    </>
+                  )}
+
+                  {/* experience */}
+                  {"name" in item && item.name !== null && (
+                    <>
+                      <div className="grid grid-cols-2 gap-x-20 border-b border-gray-400 justify-center max-w-[75%] xl:max-w-[90%] md:max-w-[50%] xl:mx-0 mx-auto">
+                        <div className="flex my-4 gap-4">
+                          <Image
+                            src={item.img_url}
+                            alt={item.name}
+                            width={50}
+                            height={50}
+                            className="mb-4"
+                          />
+                          <div className="grid justify-center items-center h-full">
+                            <div className="flex justify-between gap-16 w-[26rem] h-full">
+                              <p className="font-bold text-white">
+                                {item.name}
+                              </p>
+                              <Link href={item.link} target="_blank">
+                                <FaArrowTrendUp className="h-4 w-4 text-gray-500" />
+                              </Link>
+                            </div>
+                            <div className="flex justify-between gap-16 w-[26rem] h-full">
+                              <p className="text-gray-400 text-xs">
+                                {item.position}
+                              </p>
+                              <p className="text-gray-400 text-xs">
+                                {item.started}
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                  )}
+
+                  {/* educations */}
+                  {"school" in item && item.school !== null && (
+                    <>
+                      <div className="border-b border-gray-400">
+                        <div className="flex gap-4 my-4">
+                          <div className="grid items-center h-full xl:w-[36rem] w-[30rem]">
+                            <p className="text-white font-bold">
+                              {item.school}
+                            </p>
+                            <p className="text-gray-400 text-md font-semibold">
+                              {item.years}
+                            </p>
+                            <p className="text-gray-400 text-md font-normal">
+                              {item.section}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                  )}
+
+                  {/* organization */}
+                  {"period_organization" in item &&
+                    item.period_organization !== null && (
+                      <>
+                        <ul className="xl:list-disc list-none mx-2">
+                          <li className="text-white font-semibold xl:mt-0 mt-4">
+                            {item.period_organization}
+                          </li>
+
+                          <ol className="xl:list-decimal list-none list-inside">
+                            {item.info.map((infoItem, infoIndex) => (
+                              <li key={infoIndex}>{infoItem}</li>
+                            ))}
+                          </ol>
+                        </ul>
+                      </>
+                    )}
                 </div>
-              )}
-            </div>
-          </>
-        )}
-
-        {/* experience */}
-        {item.name && (
-          <>
-            <div className="grid grid-cols-2 gap-x-20 border-b border-gray-400 justify-center max-w-[75%] xl:max-w-[90%] md:max-w-[50%] xl:mx-0 mx-auto">
-              <div className="flex my-4 gap-4">
-                <Image
-                  src={item.img_url}
-                  alt={item.name}
-                  width={50}
-                  height={50}
-                  className="mb-4"
-                />
-                <div className="grid justify-center items-center h-full">
-                  <div className="flex justify-between gap-16 w-[26rem] h-full">
-                    <p className="font-bold text-white">{item.name}</p>
-                    <Link href={item.link} target="_blank">
-                      <FaArrowTrendUp class="h-4 w-4 text-gray-500" />
-                    </Link>
-                  </div>
-                  <div className="flex justify-between gap-16 w-[26rem] h-full">
-                    <p className="text-gray-400 text-xs">{item.position}</p>
-                    <p className="text-gray-400 text-xs">{item.started}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </>
-        )}
-
-        {/* educations */}
-        {item.school && (
-          <>
-           <div className="border-b border-gray-400">
-            <div className="flex gap-4 my-4">
-              <div className="grid items-center h-full xl:w-[36rem] w-[30rem]">
-                 <p className="text-white font-bold">{item.school}</p>
-                    <p className="text-gray-400 text-md font-semibold">{item.years}</p>
-                    <p className="text-gray-400 text-md font-normal">{item.section}</p>
-              </div>
-            </div>
-
-           </div>
-          </>
-        )}
-
-
-        {/* organization */}
-        {item.period_organization && (
-          <>
-         <ul class="xl:list-disc list-none mx-2">
-            <li className="text-white font-semibold xl:mt-0 mt-4">{item.period_organization}</li>
-
-          <ol className="xl:list-decimal list-none list-inside ">
-        {item.info.map((infoItem, infoIndex) => (
-          <li key={infoIndex} >{infoItem}</li>
-        ))}
-      </ol>
-        </ul>
-          </>
-        )}
-      </div>
-    );
-          })}
+              );
+            })}
           </div>
         </motion.div>
       </div>
