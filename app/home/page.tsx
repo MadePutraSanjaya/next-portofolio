e8d9af67c62dffe3953c724380e91e57c509431c
MadePutraSanjaya <madeputra.6166@gmail.com>
2024-03-06 15:06:56 +0800

    fix: desktop home page responsive

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -11,7 +11,7 @@ import ProfileMain from "../../components/ProfileMain/ProfileMain";
 
 const Home = () => {
   return (
-    <div className="overflow-x-hidden xl:py-0 md:py-32 py-32 bg-primary/60 h-full w-screen">
+    <div className="overflow-x-hidden xl:py-0 md:py-32 py-32 bg-primary/60 h-screen w-screen">
       <div className="w-full h-full xl:bg-gradient-to-r xl:from-primary/10 xl:via-black/30 xl:to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           <motion.h1
