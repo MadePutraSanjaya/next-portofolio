1421ba8e6403ed3c7cd5786767707821d6001dc7
MadePutraSanjaya <madeputra.6166@gmail.com>
2024-03-08 06:07:51 +0800

    fix: remove unused type

--- app/types/button.type.ts
+++
@@ -1,6 +0,0 @@
-export interface Button {
-    children?: React.ReactNode;
-    className?: React.ReactNode;
-    onClick?: () => void;
-    style?: React.CSSProperties;
-}
--- app/types/cardInfo.type.ts
+++
@@ -1,6 +0,0 @@
-interface CardInfo {
-    img_url: string;
-    title: string;
-    subTitle: string;
-    href: string;
-}
