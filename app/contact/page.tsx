b8318545a1cebee974e408b2d7078e499256cf09
MadePutraSanjaya <madeputra.6166@gmail.com>
2024-03-07 23:05:57 +0800

    feature: adjustment env contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, {useEffect, useRef, useState} from 'react'
+import React, {useEffect, useRef, FormEvent, useState} from 'react'
 import { motion } from "framer-motion"
 import { fadeIn } from "../../variant"
 import { RiSendPlane2Fill } from "react-icons/ri";
@@ -9,40 +9,41 @@ import Swal from "sweetalert2";
 import "sweetalert2/src/sweetalert2.scss";
 
 const Contact = () => {
- const emailRef = useRef<HTMLInputElement>(null);
- const nameRef = useRef<HTMLInputElement>(null);
- const subjectRef = useRef<HTMLInputElement>(null);
- const messageRef = useRef<HTMLTextAreaElement>(null);
- const formRef = useRef<HTMLFormElement>(null);
+ const form = useRef<HTMLFormElement | null>(null);
  const [loading, setLoading] = useState(false);
 
-  useEffect(() => emailjs.init("zuBynHZamUUqjsbah"), []);
+  const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID as string;
+  const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID as string;
+  const userId = process.env.NEXT_PUBLIC_EMAILJS_USER_ID as string;
 
-     const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    const serviceId = "service_v2b23a6";
-    const templateId = "template_b8muu1m";
-    try {
-      setLoading(true);
-     await emailjs.send(serviceId, templateId, {
-       name: nameRef?.current?.value ?? "",
-       recipient: emailRef?.current?.value ?? "",
-       subject: subjectRef?.current?.value ?? "",
-       message: messageRef?.current?.value ?? "",
-     });
+ const handleSubmit = async (e: any) => {
+      e.preventDefault();
 
-     Swal.fire({
+  try {
+    setLoading(true);
+    const currentForm = form.current;
+    if (currentForm) {
+      await emailjs.sendForm(serviceId, templateId, currentForm, userId);
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "The form is empty, try fill it out first",
+      });
+    }
+
+   Swal.fire({
        title: "Your email has been sent!",
        text: "Wait for the response, OK!",
        icon: "success",
      });
-    formRef.current?.reset();
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+    form.current?.reset();
+  } catch (error) {
+    console.log(error);
+  } finally {
+    setLoading(false);
+  }
+}
 
   return (
     <div className="h-screen bg-primary/30">
@@ -58,7 +59,7 @@ const Contact = () => {
             Let’s <span className="text-accent">connect.</span>
           </motion.h2>
           <motion.form
-            ref={formRef}
+            ref={form}
             onSubmit={handleSubmit}
              variants={fadeIn("up", 0.4)}
               initial="hidden"
@@ -69,32 +70,36 @@ const Contact = () => {
             <div className="flex gap-x-6 w-full">
               <input
                 className="input capitalize"
-                ref={nameRef}
                 type="text"
                 required
+                id="" 
+                name="name"
                 placeholder="Name"
               />
               <input
                 className="input"
-                ref={emailRef}
                 type="text"
                 required
+                id="" 
+                name="recipient"
                 placeholder="Email"
               />
             </div>
             <input
               className="input capitalize"
-              ref={subjectRef}
               type="text"
               required
+              id="" 
+              name="subject"
               placeholder="Subject"
             />
 
             <textarea
               className="textarea"
-              ref={messageRef}
               placeholder="Message"
               required
+              id="" 
+              name="message"
             ></textarea>
             <button className="btn rounded-full bg-accent max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden group">
               <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
