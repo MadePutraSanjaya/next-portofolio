9621683408c7db27221befae00b27b5df6619086
I MADE ADI PUTRA SANJAYA <madeputra.6166@gmail.com>
2024-03-11 19:43:07 +0800

    fix: link blank

--- components/Social/Social.tsx
+++ components/Social/Social.tsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React from "react";
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
-import Link from 'next/link';
+import Link from "next/link";
 
 const Social = () => {
   return (
     <div className="flex items-center gap-x-5 text-lg">
-     <Link className="hover:text-accent transition-all duration-300" href="https://github.com/MadePutraSanjaya">
+      <Link
+        className="hover:text-accent transition-all duration-300"
+        target="_blank"
+        href="https://github.com/MadePutraSanjaya"
+      >
         <FaGithub />
-     </Link>
-       <Link className="hover:text-accent transition-all duration-300" href="https://www.linkedin.com/in/madesanjaya/">
-          <FaLinkedin />
-     </Link>
-       <Link className="hover:text-accent transition-all duration-300" href="https://www.instagram.com/mdsanjaya._/">
-     <FaInstagram />
-     </Link>
+      </Link>
+      <Link
+        className="hover:text-accent transition-all duration-300"
+        target="_blank"
+        href="https://www.linkedin.com/in/madesanjaya/"
+      >
+        <FaLinkedin />
+      </Link>
+      <Link
+        className="hover:text-accent transition-all duration-300"
+        target="_blank"
+        href="https://www.instagram.com/mdsanjaya._/"
+      >
+        <FaInstagram />
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Social
+export default Social;
