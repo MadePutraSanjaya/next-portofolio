ca6b4d970df7e446a5181c6a0039a71c997d1db9
I MADE ADI PUTRA SANJAYA <madeputra.6166@gmail.com>
2024-03-11 19:19:58 +0800

    fix: issue social link

--- components/Social/Social.tsx
+++ components/Social/Social.tsx
@@ -5,13 +5,13 @@ import Link from 'next/link';
 const Social = () => {
   return (
     <div className="flex items-center gap-x-5 text-lg">
-     <Link className="hover:text-accent transition-all duration-300" href="">
+     <Link className="hover:text-accent transition-all duration-300" href="https://github.com/MadePutraSanjaya">
         <FaGithub />
      </Link>
-       <Link className="hover:text-accent transition-all duration-300" href="">
+       <Link className="hover:text-accent transition-all duration-300" href="https://www.linkedin.com/in/madesanjaya/">
           <FaLinkedin />
      </Link>
-       <Link className="hover:text-accent transition-all duration-300" href="">
+       <Link className="hover:text-accent transition-all duration-300" href="https://www.instagram.com/mdsanjaya._/">
      <FaInstagram />
      </Link>
     </div>
